9828429a7f475b29b0d7c660841ecba037843ee9
Andreas Wachs <andreas@wachs.dk>
2022-08-28 16:59:38 +0200

    Clean up the main file

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,11 @@ mod workflows;
 
 #[tokio::main]
 async fn main() -> Result<(), isize> {
-    println!("main()");
     // Load the config file
     let config = config::Config::load();
 
     // Load the workflows
     workflows::load(&config).await;
 
-    println!("hello world from main");
     Ok(())
 }
